7ee92b33fd4fff51145bf25ebe55935e2fbe4a97
nittics1991 <bunpabcom@gmail.com>
2020-03-14 15:09:51 +0900

    phplint

---
+++ bin/phplint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd $(dirname "$0")
+
+find ../src -name *.php \
+    |xargs -n 1 php -l  \
+    |grep -e "No syntax errors detected" -v
+
+#grepはtarget発見でreturn 0
+
+if [[ "$?" == 1 ]] ; then
+    exit 0
+else
+    exit 1
+fi
