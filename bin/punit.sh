e15bc4772612f2ed7aa21d49da5d64ceeded20bf
nittics1991 <bunpabcom@gmail.com>
2020-03-14 14:25:12 +0900

    phpcs error

--- bin/punit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CWD=$(cd $(dirname "$0");pwd)
-
-cd "$CWD"
-
-phpunit -c "../phpunit.xml" > ../log/phpunit.log 2>&1
---
+++ bin/setting_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd $(dirname "$0")
+
+source ../env.list
+
+#sudo apt install -y ${TOOLS[@]}
+sudo apt install ${TOOLS[@]}
