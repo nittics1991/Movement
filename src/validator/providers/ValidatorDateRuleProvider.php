e1b58e05c890a1ebd449da795f917eac9e1013ff
nittics1991 <bunpabcom@gmail.com>
2020-07-05 14:10:09 +0900

    validate date rule test 見直し中

--- src/validator/providers/ValidatorDateRuleProvider.php
+++ src/validator/providers/ValidatorDateRuleProvider.php
@@ -30,10 +30,19 @@ class ValidatorDateRuleProvider extends AbstractValidatorRuleProvider
                 return $formatted == $v;
             },
             'dateEqual' => function($v, $f, $t) {
-                if (!($date = date_create_from_format("!{$f}", $v))) {
-                    return false;
-                }
-                if (!($target = date_create_from_format("!{$f}", $t))) {
+                list($date, $target) = $this->toDateTime($f, $v, $t);
+                
+                if (!$date || !$target) {
+                    
+                    
+                    var_dump($v);
+                    var_dump($t);
+                    var_dump($f);
+                    var_dump($date);
+                    var_dump($target);
+                    var_dump("-----------------");
+                    
+                    
                     return false;
                 }
                 return $date == $target;
@@ -44,4 +53,22 @@ class ValidatorDateRuleProvider extends AbstractValidatorRuleProvider
             
         ];
    }
+   
+    /**
+    *   toDateTime
+    *
+    *   @param string $format
+    *   @param string $value1
+    *   @param string $value2
+    *   @return DateTime|bool[]
+    */
+    protected function toDateTime(
+        string $format,
+        string $value1,
+        string $value2
+    ): array {
+        $date1 = date_create_from_format("!{$value1}", $format);
+        $date2 = date_create_from_format("!{$value2}", $format);
+        return [$date1, $date2];
+    }
 }
